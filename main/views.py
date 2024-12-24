31e61b3973618097c42f7566d6711f39f54cb160
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2024-12-24 15:03:53 +0200

    Initialized main app with index and about pages

--- main/views.py
+++ main/views.py
@@ -4,14 +4,16 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'This is the shop home page - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False,
+        'title': 'Home - Головна',
+        'content': 'Магазин меблів HOME',
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        'title': 'Home - Про нас',
+        'content': 'Про нас',
+        'text_on_page': 'Текст про те чому цей магазин такий класний, і який гарний товар'
+    }
+    return render(request, 'main/about.html', context)
