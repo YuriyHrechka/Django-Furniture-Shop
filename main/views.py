2cadd83a3ba009575d5bd997a880bcf96a071236
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2025-01-14 13:35:51 +0200

    ADDED: registered simple tag for categories

--- main/views.py
+++ main/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from goods.models import Categories
-
 
 def index(request):
-    categories = Categories.objects.all()
-
     context = {
         'title': 'Home - Головна',
         'content': 'Магазин меблів HOME',
-        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
