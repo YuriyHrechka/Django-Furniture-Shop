428caf213e9e51be3c8687850c5f0d76b705135d
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2025-01-28 13:30:22 +0200

    ADDED: login required decorator, redirect to target page

--- users/views.py
+++ users/views.py
@@ -17,6 +17,9 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{user.username}, Ви ввійшли в акаунт')
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -64,6 +67,7 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
+@login_required
 def logout(request):
     messages.success(request, f'{request.user.username}, Ви вийшли з акаунта')
     auth.logout(request)
