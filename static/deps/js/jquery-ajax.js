f277a1d92f186ac95f20daaa3a219b3437f45488
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2025-02-01 12:08:02 +0200

    ADDED: change-cart ajax and json response

--- static/deps/js/jquery-ajax.js
+++ static/deps/js/jquery-ajax.js
@@ -101,79 +101,79 @@ $(document).ready(function () {
     });
 
 
-    // // Тепер + - кількості товару
-    // // Обробник події для зменшення значення
-    // $(document).on("click", ".decrement", function () {
-    //     // Беремо посилання на контролер django з атрибута data-cart-change-url
-    //     var url = $(this).data("cart-change-url");
-    //     // Беремо id кошика з атрибута data-cart-id
-    //     var cartID = $(this).data("cart-id");
-    //     // Шукаємо найближчий input з кількістю
-    //     var $input = $(this).closest('.input-group').find('.number');
-    //     // Беремо значення кількості товару
-    //     var currentValue = parseInt($input.val());
-    //     // Якщо кількість більше одного, то тільки тоді робимо -1
-    //     if (currentValue > 1) {
-    //         $input.val(currentValue - 1);
-    //         // Запускаємо функцію, визначену нижче
-    //         // з аргументами (id кошика, нова кількість, кількість зменшилася чи збільшилася, url)
-    //         updateCart(cartID, currentValue - 1, -1, url);
-    //     }
-    // });
+    // Тепер + - кількості товару
+    // Обробник події для зменшення значення
+    $(document).on("click", ".decrement", function () {
+        // Беремо посилання на контролер django з атрибута data-cart-change-url
+        var url = $(this).data("cart-change-url");
+        // Беремо id кошика з атрибута data-cart-id
+        var cartID = $(this).data("cart-id");
+        // Шукаємо найближчий input з кількістю
+        var $input = $(this).closest('.input-group').find('.number');
+        // Беремо значення кількості товару
+        var currentValue = parseInt($input.val());
+        // Якщо кількість більше одного, то тільки тоді робимо -1
+        if (currentValue > 1) {
+            $input.val(currentValue - 1);
+            // Запускаємо функцію, визначену нижче
+            // з аргументами (id кошика, нова кількість, кількість зменшилася чи збільшилася, url)
+            updateCart(cartID, currentValue - 1, -1, url);
+        }
+    });
 
-    // // Обробник події для збільшення значення
-    // $(document).on("click", ".increment", function () {
-    //     // Беремо посилання на контролер django з атрибута data-cart-change-url
-    //     var url = $(this).data("cart-change-url");
-    //     // Беремо id кошика з атрибута data-cart-id
-    //     var cartID = $(this).data("cart-id");
-    //     // Шукаємо найближчий input з кількістю
-    //     var $input = $(this).closest('.input-group').find('.number');
-    //     // Беремо значення кількості товару
-    //     var currentValue = parseInt($input.val());
+    // Обробник події для збільшення значення
+    $(document).on("click", ".increment", function () {
+        // Беремо посилання на контролер django з атрибута data-cart-change-url
+        var url = $(this).data("cart-change-url");
+        // Беремо id кошика з атрибута data-cart-id
+        var cartID = $(this).data("cart-id");
+        // Шукаємо найближчий input з кількістю
+        var $input = $(this).closest('.input-group').find('.number');
+        // Беремо значення кількості товару
+        var currentValue = parseInt($input.val());
 
-    //     $input.val(currentValue + 1);
+        $input.val(currentValue + 1);
 
-    //     // Запускаємо функцію, визначену нижче
-    //     // з аргументами (id кошика, нова кількість, кількість зменшилася чи збільшилася, url)
-    //     updateCart(cartID, currentValue + 1, 1, url);
-    // });
+        // Запускаємо функцію, визначену нижче
+        // з аргументами (id кошика, нова кількість, кількість зменшилася чи збільшилася, url)
+        updateCart(cartID, currentValue + 1, 1, url);
+    });
 
-    // function updateCart(cartID, quantity, change, url) {
-    //     $.ajax({
-    //         type: "POST",
-    //         url: url,
-    //         data: {
-    //             cart_id: cartID,
-    //             quantity: quantity,
-    //             csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
-    //         },
+    function updateCart(cartID, quantity, change, url) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                cart_id: cartID,
+                quantity: quantity,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
 
-    //         success: function (data) {
-    //              // Повідомлення
-    //             successMessage.html(data.message);
-    //             successMessage.fadeIn(400);
-    //              // Через 7 сек приховуємо повідомлення
-    //             setTimeout(function () {
-    //                  successMessage.fadeOut(400);
-    //             }, 7000);
+            success: function (data) {
+                 // Повідомлення
+                successMessage.html(data.message);
+                successMessage.fadeIn(400);
+                 // Через 7 сек приховуємо повідомлення
+                setTimeout(function () {
+                     successMessage.fadeOut(400);
+                }, 7000);
 
-    //             // Змінюєм кількість товарів в кошику
-    //             var goodsInCartCount = $("#goods-in-cart-count");
-    //             var cartCount = parseInt(goodsInCartCount.text() || 0);
-    //             cartCount += change;
-    //             goodsInCartCount.text(cartCount);
+                // Змінюєм кількість товарів в кошику
+                var goodsInCartCount = $("#goods-in-cart-count");
+                var cartCount = parseInt(goodsInCartCount.text() || 0);
+                cartCount += change;
+                goodsInCartCount.text(cartCount);
 
-    //             // Міняєм вміст кошика
-    //             var cartItemsContainer = $("#cart-items-container");
-    //             cartItemsContainer.html(data.cart_items_html);
+                // Міняєм вміст кошика
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
 
-    //         },
-    //         error: function (data) {
-    //             console.log("Помилка при додаванні товару до кошика");
-    //         },
-    //     });
-    // }
+            },
+            error: function (data) {
+                console.log("Помилка при додаванні товару до кошика");
+            },
+        });
+    }
 
     // Беремо з розмітки елемент по id - сповіщення від django
     var notification = $('#notification');
