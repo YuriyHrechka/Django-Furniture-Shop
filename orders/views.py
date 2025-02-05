fbc1ffd8a6e726b9aa7e0106a4e7ff1025b8f8c8
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2025-02-05 22:27:35 +0200

    ADDED: show user order history CHANGED: small changes

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.shortcuts import render, redirect
@@ -8,6 +9,7 @@ from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
 
 
+@login_required
 def create_order(request):
     if request.method == 'POST':
         form = CreateOrderForm(data=request.POST)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from carts.models import Cart
+from orders.models import OrderItem, Order
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
@@ -76,9 +78,19 @@ def profile(request):
             return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
+
+    orders = (
+        Order.objects.filter(user=request.user).prefetch_related(
+            Prefetch(
+                'orderitem_set',
+                queryset=OrderItem.objects.select_related('product'),
+            )
+        ).order_by('-id')
+    )
     context = {
         'title': 'Home - Кабінет',
         'form': form,
+        'orders': orders,
     }
     return render(request, 'users/profile.html', context)
 
