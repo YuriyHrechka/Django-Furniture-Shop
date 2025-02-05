660c066c006c4b398968f0bcaa2e91bd967c6ddc
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2025-02-05 13:01:17 +0200

    ADDED: Admin panel setup

--- carts/models.py
+++ carts/models.py
@@ -33,4 +33,7 @@ class Cart(models.Model):
         return round(self.product.sell_price() * self.quantity, 2)
 
     def __str__(self):
-        return f'Кошик {self.user.username} | Товар {self.product.name} | Кількість {self.quantity}'
+        if self.user:
+            return f'Кошик {self.user.username} | Товар {self.product.name} | Кількість {self.quantity}'
+        else:
+            return f'Анонімний кошик | Товар {self.product.name} | Кількість {self.quantity}'
